2d91e63dd8364cea7c2059c559f58dc60157fb45
leespen1 <leespen1@msu.edu>
2021-09-03 17:27:30 -0400

    Revised graphical output, cleaned up code, switched to Gauss-Lobatto nodes, changed structure to promote code reuse.

--- spectral_deferred_correction_dahlquist.jl
+++ spectral_deferred_correction_dahlquist.jl
@@ -9,176 +9,83 @@ SymPy = PyCall.pyimport("sympy")
 import FastGaussQuadrature as FGQ # So I don't have to calculate Gauss-Legendre nodes myself, although I should be able to
 #import BenchmarkTools
 
+struct Parameters{T}
+    λ::Float64
+    Δt::Float64
+    t_n::Float64
+    c_nodes::Vector{Float64}
+    times::Vector{Float64}
+    Δt_0::Float64
+    Δtimes::Vector{Float64}
+    S::Matrix{Float64}
+    u_n::T
+end
+
+function Parameters(λ::Float64, Δt::Float64, t_n::Float64, c_nodes::Vector{Float64})
+    return Parameters(
+        λ,
+        Δt,
+        t_n,
+        c_nodes,
+        map(x -> t_n + Δt*x, c_nodes),# Times
+        c_nodes[1]*Δt,# Δt_0
+        map(i -> (c_nodes[i+1]-c_nodes[i])*Δt, 1:length(c_nodes)-1),# Δtimes
+        spectral_integration_matrix(c_nodes),# S
+        exp(λ*t_n),# u_n
+    )
+end
 
 """
 Main
 """
-function main()
-    # Function Paramaters
-    λ = 0.7
+function main(λ::Float64, Δt::Float64, Δt_n::Float64, c_nodes::Vector{Float64})
     u(t) = exp(λ*t)
     f(t, u) = λ*u
 
-    # Initial Conditions / Approximation Parameters
-    #c_nodes = range(0, 1, length=10) # Old method, equally spaced nodes
-    c_nodes, weights =  FGQ.gausslegendre(10)
-    # Shift from [-1,1] the the standard interval [0,1]
-    for (i, node) in enumerate(c_nodes)
-        c_nodes[i] = (node+1)/2
-    end
-    validate_collocation_nodes(c_nodes)
+    para = Parameters(λ, Δt, t_n, c_nodes)
 
-    start_t = 1.7
-    u_n = u(start_t)
-    Δt = 2.5
-
-
-    times = map(x -> start_t + Δt*x, c_nodes)
-    Δt_0 = c_nodes[1]*Δt
-    Δtimes = map(i -> (c_nodes[i+1]-c_nodes[i])*Δt, 1:length(c_nodes)-1)
-    validate_time_differences(λ, Δt_0, Δtimes)
+    # Validation
+    validate_collocation_nodes(para)
+    validate_time_differences(para)
 
     p = no_of_points = length(c_nodes)
     no_of_corrections = 4*p
 
-    S = spectral_integration_matrix(c_nodes)
-
-    # Get exact values for comparison purposes
-    u_vec_exact = [u(t) for t in times]
-
-    # Initialize vectors for approximations we will want to save
-    u_vec_1 = Vector{Float64}(undef, length(c_nodes))
-    u_vec_2 = Vector{Float64}(undef, length(c_nodes))
-    u_vec_p = Vector{Float64}(undef, length(c_nodes))
-    u_vec_2p = Vector{Float64}(undef, length(c_nodes))
-    u_vec_4p = Vector{Float64}(undef, length(c_nodes))
-
     # Prediction Phase - Standard backward Euler to fill first column
-    u_vec_1[1] =backward_euler_step(λ, Δt_0, u_n)
+    u_vec_1 = Vector{Float64}(undef, p) # Will need to change to accomodate higher dimensions
+    u_vec_1[1] = backward_euler_step(para.λ, para.Δt_0, para.u_n)
     for i in 1:length(u_vec_1)-1
-        u_vec_1[i+1] = backward_euler_step(λ, Δtimes[i], u_vec_1[i])
+        u_vec_1[i+1] = backward_euler_step(para.λ, para.Δtimes[i], u_vec_1[i])
     end
 
     u_vec_k = u_vec_1[:] # Copy first approximation
-    u_vec_kp1 = Vector{Float64}(undef, length(c_nodes))
+    # Correction Phase
+    u_np1_saves = correction_phase!(u_vec_k, para, 2*p, saves=1:2*p)
+    pushfirst!(u_np1_saves, (0, u_vec_1[end]))
 
-    ## Correction Phase - No animation
-    #for i in 1:no_of_corrections
-    #    make_correction!(u_vec_k, u_vec_kp1, u_n, λ, S, Δt_0, Δtimes, Δt)
-    #    swap!(u_vec_k, u_vec_kp1)
-    #    # Save a selection of the correction process
-    #    if i == 2
-    #        u_vec_2 .= u_vec_k[:]
-    #    elseif i == p
-    #        u_vec_p .= u_vec_k[:]
-    #    elseif i == 2*p
-    #        u_vec_2p .= u_vec_k[:] # Save Copy of 2pth Correction
-    #    elseif i == 4*p
-    #        u_vec_4p .= u_vec_k[:]
-    #    end
-    #end
-
-    # Correction Phase - With animation
-    animation = Plots.@animate for i in 1:no_of_corrections
-        make_correction!(u_vec_k, u_vec_kp1, u_n, λ, S, Δt_0, Δtimes, Δt)
-        swap!(u_vec_k, u_vec_kp1)
-
-        errors_pred = [abs(u) for u in u_vec_exact - u_vec_1]
-        errors_corr = [abs(u) for u in u_vec_exact - u_vec_k]
-        plot = Plots.plot(
-            errors_pred[2:end],
-            label="Prediction",
-        )
-        Plots.plot!(
-            errors_corr[2:end],
-            label = "Correction - K = $i"
-        )
-        # Save a selection of the correction process
-        if i == 2
-            u_vec_2 .= u_vec_k[:]
-        elseif i == p
-            u_vec_p .= u_vec_k[:]
-        elseif i == 2*p
-            u_vec_2p .= u_vec_k[:]
-        elseif i == 4*p
-            u_vec_4p .= u_vec_k[:]
-        end
+    # Get exact values for comparison purposes
+    u_exact = u(t_n+Δt)
+    # Compute errors
+    u_np1_saves_errors = Vector{Tuple{Int64, Float64}}(undef, length(u_np1_saves))
+    for (i, (j, u_np1)) in enumerate(u_np1_saves)
+        u_np1_saves_errors[i] = (j, abs(u_np1-u_exact))
     end
-    fps = floor(no_of_corrections/5) # fps needed for a 5 second animation
-    fps = Int(min(fps, 30)) # Don't exceed 30 fps (to keep easy to interpret, should maybe use lower floor)
-    Plots.gif(animation, "spectral_deferred_correction_evolution.gif", fps=fps)
 
-    # At this point, u_vec_1 holds the prediction, and u_vec_k holds the most
-    # recent revision. If I want to analyze the evolution of the revisions, I
-    # would have to save them during the correction phase.
-    get_errors(u_vec) = [abs(u) for u in u_vec_exact - u_vec]
-    errors_to_include = [get_errors(u_vec) for u_vec in 
-        (
-         u_vec_1,
-         u_vec_2,
-         u_vec_p,
-         u_vec_2p,
-         u_vec_4p,
-        )
-    ]
-
-    ## In case I want to look at the fractional error.
-    #frac_errors_pred = [abs((u_est-u_ex)/u_ex) for (u_est, u_ex) in zip(u_vec_1, u_vec_exact)]
-    #frac_errors_corr = [abs((u_est-u_ex)/u_ex) for (u_est, u_ex) in zip(u_vec_2p, u_vec_exact)]
-
-    ## For looking at some of the results by hand
-    ##sample(u) = u[1:Int(floor(p/5)):end]
-    #sample(u) = u
-    ##println("\nS - Spectral Integration Matrix")
-    ##display(S)
-    #println("\nCollocation Nodes")
-    #display(c_nodes)
-    #println("\nExact Values")
-    #display(sample(u_vec_exact))
-    #println("\nFirst Prediction")
-    #display(sample(u_vec_1))
-    #println("\nFinal Revision")
-    #display(sample(u_vec_k))
-    #println("\nErrors Prediction")
-    #display(sample(errors_pred))
-    #println("\nErrors Correction")
-    #display(sample(errors_corr_2p))
-    #println()
-    
     # Plotting
-    Δtimes_plot = map(t -> t - start_t, times[2:end])
-    errors_to_include = map(x -> x[2:end], errors_to_include)
-
-    display(Δtimes_plot)
-    display(errors_to_include)
-
-    plot2 = Plots.plot(
-        #Δtimes_plot,
-        errors_to_include,
-        label=hcat(
-            "Prediction",
-            "K=2",
-            "K=p",
-            "K=2p",
-            "K=4p",
-        )
+    plot = Plots.plot(
+        getindex.(u_np1_saves_errors, 1), # No of Corrections
+        getindex.(u_np1_saves_errors, 2), # Error
+        label="Approximation of u(t_n+Δt)"
     )
-
-    # Get list of all errors (to set y limits of plot)
-    all_errors = reduce(vcat, errors_to_include)
     Plots.plot!(
-        plot2,
-        title="Error Correction Analysis - λ=$λ",
-        #xticks=Δtimes_plot,
-        xlabel="# of Steps (Δt = $Δt, t_n = $start_t)",
+        title="Errors Over 1 Timestep Vs No of Corrections \nλ=$λ, Δt=$Δt, t_n=$t_n, p=$p (Gauss-Lobatto)",
+        xlabel="# of Corrections (0 ⟹ Backward Euler Prediction)",
         ylabel="Abs(E)",
-        ylim=(minimum(all_errors), maximum(all_errors)),
-        yaxis=:log,
-        marker=:cross,
+        #yaxis=:log,
     )
 
-    Plots.savefig(plot2, "spectral_deferred_correction_evolution.png")
-    display(plot2)
+    Plots.savefig(plot, "spectral_deferred_correction_evolution.png")
+    display(plot)
     return
 end
 
@@ -249,6 +156,10 @@ function make_correction!(u_vec_k, u_vec_kp1, u_n, λ::Float64, S::Matrix{Float6
     end
 end
 
+function make_correction!(u_vec_k, u_vec_kp1, p::Parameters)
+    make_correction!(u_vec_k, u_vec_kp1, p.u_n, p.λ, p.S, p.Δt_0, p.Δtimes, p.Δt)
+end
+
 
 """
 Implementation of equation (B15)
@@ -260,6 +171,10 @@ function eq_b15!(u_vec_k, u_vec_kp1, λ::Float64, S::Matrix{Float64}, Δt_0::Flo
     u_vec_kp1[1] = factor1*factor2
 end
 
+function eq_b15!(u_vec_k, u_vec_kp1, p::Parameters)
+    eq_b15!(u_vec_k, u_vec_kp1, p.λ, p.S, p.Δt_0, p.Δt, p.u_n)
+end
+
 
 """
 Implementation of equation (B16)
@@ -269,6 +184,10 @@ function eq_b16!(u_vec_k, u_vec_kp1, λ::Float64, S::Matrix{Float64}, Δt_m::Flo
     factor1 = (1/(1-Δt_m*λ))
     factor2 = u_vec_kp1[m] - Δt_m*λ*u_vec_k[m+1] + Δt*sum(summand,1:length(u_vec_k))
     u_vec_kp1[m+1] = factor1*factor2
+end
+
+function eq_b16!(u_vec_k, u_vec_kp1, p::Parameters, m::Int64)
+    eq_b16!(u_vec_k, u_vec_kp1, p.λ, p.S, p.Δtimes[m], p.Δt, m)
 end
 
 
@@ -286,6 +205,10 @@ function validate_collocation_nodes(c_nodes)
             throw(DomainError(node, "Collocation nodes must be obey c_m+1 > c_m ∀ m = 1, ... , p."))
         end
     end
+end
+
+function validate_collocation_nodes(p::Parameters)
+    validate_collocation_nodes(p.c_nodes)
 end
 
 
@@ -306,6 +229,10 @@ function validate_time_differences(λ::Float64, Δt_0::Float64, Δtimes::Vector{
     end
 end
 
+function validate_time_differences(p::Parameters)
+    validate_time_differences(p.λ, p.Δt_0, p.Δtimes)
+end
+
 
 """
 Swap contents of two vectors (or iterables in general) quickly and without
@@ -317,4 +244,54 @@ function swap!(a_vec, b_vec)
     end
 end
 
-main()
+function correction_phase!(
+        u_vec_k, u_n,
+        λ::Float64, S::Matrix{Float64}, Δt_0::Float64, Δtimes::Vector{Float64}, Δt::Float64,
+        no_of_corrections::Int64; saves=[])
+
+    u_vec_kp1 = u_vec_k[:]
+
+    return_vec = []
+    # Correction Phase - No animation
+    for i in 1:no_of_corrections
+        make_correction!(u_vec_k, u_vec_kp1, u_n, λ, S, Δt_0, Δtimes, Δt)
+        swap!(u_vec_k, u_vec_kp1)
+        # Save a selection of the correction process
+        if i in saves
+            push!(return_vec, (i, u_vec_k[end]))
+        end
+    end
+    if !(no_of_corrections in saves) # At least return the latest correction
+        push!(return_vec, (no_of_corrections, u_vec_k[end]))
+    end
+    return return_vec
+end
+
+function correction_phase!(u_vec_k, p::Parameters,
+        no_of_corrections::Int64; saves=[])
+
+    correction_phase!(
+        u_vec_k,
+        p.u_n,
+        p.λ,
+        p.S,
+        p.Δt_0,
+        p.Δtimes,
+        p.Δt,
+        no_of_corrections,
+        saves=saves
+    )
+end
+
+
+λ = -1.0
+Δt = 1.0
+t_n = 0.0
+c_nodes, weights =  FGQ.gausslobatto(7)
+# Shift from [-1,1] the the standard interval [0,1]
+for (i, node) in enumerate(c_nodes)
+    c_nodes[i] = (node+1)/2
+end
+
+main(λ, Δt, t_n, c_nodes)
+
