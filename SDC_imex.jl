821bc2c74da33765eb8e27916fca8c484964b3b9
leespen1 <leespen1@msu.edu>
2021-11-08 16:31:03 -0500

    Major clean-up/structure change

--- SDC_imex.jl
+++ SDC_imex.jl
@@ -2,289 +2,31 @@ using Polynomials, Plots, LinearAlgebra, FastGaussQuadrature,InvertedIndices
 import PyCall
 import Plots
 SymPy = PyCall.pyimport("sympy")
-import FastGaussQuadrature as FGQ # So I don't have to calculate Gauss-Legendre nodes myself, although I should be able to
+import FastGaussQuadrature as FGQ
 #import BenchmarkTools
 
-λ=-1.0
-exp_coeff = 0.0
-imp_coeff = 1.0-exp_coeff
+#"""
+#Solves the equation:
+#    w + alpha*fI(t,w) = b
+#For w given α, t, and b
+#Do I need a best guess for w, as well?
+#"""
+#function implicit_solve(α, t, b)
+#end
 
-function u(t::Float64)
-    exp(λ*t)
+"""
+Get n Gauss-Lobatto quadrature nodes, on interval [0-1].
+"""
+function gauss_lobatto_nodes(n::Int)
+    nodes, weights =  FGQ.gausslobatto(n)
+    return 0.5*(nodes .+ 1.0)
 end
 
 """
-The main definition of f. Behavior of all f-related functions (f_exp, f_imp,
-etc, will refer back to this one)
+Get the spectral integration matrix
 """
-function f(t::Float64, u)
-    λ*u
-    #λ*exp(λ*t)
-end
-
-
-function f_exp(t::Float64, u)
-    exp_coeff*f(t, u)
-end
-
-function f_imp(t::Float64, u)
-    imp_coeff*f(t, u)
-end
-
-"""
-Inverse of u-dt*f^I(t, u), in terms of u, for aid in implicit solutions.
-
-For general use, would have to handle f symbolically.
-"""
-function LHS_factor(t::Float64, dt::Float64)
-    (1-imp_coeff*dt*λ)
-end
-
-function Δf_exp(t::Float64, u_kp1, u_k)
-    f_exp(t, u_kp1)-f_exp(t,u_k)
-end
-
-
-function prediction_step(t::Float64, u, dt::Float64)
-    (u + dt*f_exp(t, u))/LHS_factor(t, dt)
-end
-
-
-function gauss_lobatto_nodes(n::Int64)
-    c_nodes, weights =  FGQ.gausslobatto(n)
-    # Shift from [-1,1] the the standard interval [0,1]
-    for (i, node) in enumerate(c_nodes)
-        c_nodes[i] = (node+1)/2
-    end
-    return c_nodes
-end
-
-
-struct Timing
-    t_n::Float64
-    Δt::Float64
-    Δt_0::Float64
-    Δtimes::Vector{Float64}
-    times::Vector{Float64}
-end
-
-
-function Timing(t_n::Float64, Δt::Float64, c_nodes::Vector{Float64})
-    Timing(
-        t_n,
-        Δt,
-        c_nodes[1]*Δt,#Δt_0
-        map(i -> Δt*(c_nodes[i+1]-c_nodes[i]), 1:length(c_nodes)-1),#Δtimes
-        map(i -> t_n+Δt*c_nodes[i], 1:length(c_nodes)),#times
-    )
-end
-
-
-function perform_timestep(u_n, T::Timing, S::Matrix{Float64})
-    p = no_of_points = length(T.Δtimes)+1
-    no_of_corrections = 2*p
-
-    # Prediction Phase
-    u_vec_1 = Vector{Float64}(undef, p) # Will need to change to accomodate higher dimensions
-    u_vec_1[1] = prediction_step(T.t_n, u_n, T.Δt_0)
-    for i in 1:length(u_vec_1)-1
-        u_vec_1[i+1] = prediction_step(T.times[i], u_vec_1[i], T.Δtimes[i])
-    end
-
-    u_vec_k = u_vec_1[:] # Copy first approximation
-
-    # Correction Phase
-    u_np1_saves = correction_phase!(u_vec_k, u_n, S, T, 2*p, saves=1:2*p)
-    pushfirst!(u_np1_saves, (0, u_vec_1[end])) # Put prediction at start of saves
-
-    return u_np1_saves
-end
-
-
-function graph(u_np1_saves, T::Timing;
-              graph_name::String="SDC_evolution.png",
-              display_plot::Bool=false)
-
-    # Get exact value for comparison purposes
-    u_exact = u(T.t_n + T.Δt)
-    # Compute errors
-    u_np1_saves_errors = Vector{Tuple{Int64, Float64}}(undef, length(u_np1_saves))
-    for (i, (j, u_np1)) in enumerate(u_np1_saves)
-        #u_np1_saves_errors[i] = (j, abs(u_np1-u_exact))
-        u_np1_saves_errors[i] = (j, u_np1 - u_exact)
-    end
-
-    # Plotting
-    plot = Plots.plot(
-        getindex.(u_np1_saves_errors, 1), # No of Corrections
-        getindex.(u_np1_saves_errors, 2), # Error
-        label="Approximation of u(t_n+Δt)"
-    )
-    p = length(T.Δtimes)+1
-    Plots.plot!(
-        plot,
-        title="Error Over 1 Timestep Vs No of Corrections \nλ=$λ, Δt=$(T.Δt), t_n=$(T.t_n), p=$p (Gauss-Lobatto)",
-        xlabel="# of Corrections (0 ⟹ Initial Prediction)",
-        ylabel="Error",
-        xlim=(0, 2*p),
-        #yaxis=:log,
-    )
-    # Make a line on y=0, for easier comprehension
-    Plots.hline!(plot, [0], label="")
-
-    Plots.savefig(plot, graph_name)
-
-    if display_plot
-        display(plot)
-    end
-    return nothing
-end
-
-"""
-Returns the spectral integration matrix for the given array of collocation nodes
-"""
-function spectral_integration_matrix(c_nodes)::Array{Float64}
-    mat_size = length(c_nodes)
-    x = SymPy.Symbol("x")
-    integrated_func(m, q) = SymPy.integrate(legrange_func(c_nodes, q), (x, 0, c_nodes[m]))
-    return [integrated_func(m, q) for m=1:mat_size, q=1:mat_size]
-end
-
-
-"""
-Returns expression (using SymPy, in terms of x) for legrange basis function
-
-Using a SymPy expression will allow me to integrate analytically (which is
-sensible, since the expression is a polynomial)
-"""
-function legrange_func(x_list, j)
-    x = SymPy.Symbol("x")
-    func = 1
-    x_j = x_list[j]
-    for (k, x_k) in enumerate(x_list)
-        if k != j
-            func = func*(x-x_k)/(x_j-x_k)
-        end
-    end
-    return func
-end
-
-
-
-"""
-Make sure collocation nodes have proper scale and order.
-"""
-function validate_collocation_nodes(c_nodes)
-    for node in c_nodes
-        if node < 0 || node > 1
-            throw(DomainError(node, "Collocation nodes must be ∈ [0,1]."))
-        end
-    end
-    for i in 1:length(c_nodes)-1
-        if c_nodes[i+1] <= c_nodes[i]
-            throw(DomainError(node, "Collocation nodes must be obey c_m+1 > c_m ∀ m = 1, ... , p."))
-        end
-    end
-end
-
-
-"""
-Swap contents of two vectors (or iterables in general) quickly and without
-allocating memory. Assumes arguments are of equal length.
-"""
-function swap!(a_vec, b_vec)
-    for i in eachindex(a_vec, b_vec)
-        a_vec[i], b_vec[i] = b_vec[i], a_vec[i]
-    end
-end
-
-
-function correction_phase!(u_vec_k, u_n, S::Matrix{Float64}, T::Timing,
-        no_of_corrections::Int64; saves=[])
-
-    u_vec_kp1 = u_vec_k[:]
-
-    return_vec = []
-    # Correction Phase - No animation
-    for i in 1:no_of_corrections
-        make_correction!(u_vec_k, u_vec_kp1, u_n,S, T)
-        swap!(u_vec_k, u_vec_kp1)
-        # Save a selection of the correction process
-        if i in saves
-            push!(return_vec, (i, u_vec_k[end]))
-        end
-    end
-    if !(no_of_corrections in saves) # At least return the latest correction
-        push!(return_vec, (no_of_corrections, u_vec_k[end]))
-    end
-
-    return return_vec
-end
-
-
-"""
-Use equations (B15) and (B16) to make a correction (k → k+1) to the current
-provided set of approximate solutions (u_vec_k).
-"""
-function make_correction!(u_vec_k, u_vec_kp1, u_n, S::Matrix{Float64}, T::Timing)
-    eq_b15!(u_vec_k, u_vec_kp1, u_n, T, S)
-    for m in 1:length(u_vec_k)-1
-        eq_b16!(u_vec_k, u_vec_kp1, T, S, m)
-    end
-end
-
-
-
-"""
-Implementation of equation (B15)
-"""
-function eq_b15!(u_vec_k, u_vec_kp1, u_n, T::Timing, S::Matrix{Float64})
-    u_vec_kp1[1] = begin
-        Δt = T.Δt
-        Δt_0 = T.Δt_0
-        times = T.times
-        t_1 = times[1]
-        u_1k = u_vec_k[1]
-
-        S_summand(q) = S[1,q]*f(T.times[q], u_vec_k[q])
-        S_sum = sum(S_summand,1:size(S)[2]) # Sum over first row of S
-        RHS_factor = u_n - Δt_0*f_imp(t_1, u_1k) + Δt*S_sum
-
-        RHS_factor/LHS_factor(t_1, Δt_0)
-    end
-end
-
-
-"""
-Implementation of equation (B16)
-"""
-function eq_b16!(u_vec_k, u_vec_kp1, T::Timing, S::Matrix{Float64}, m::Int64)
-    u_vec_kp1[m+1] = begin
-        times = T.times
-        Δt = T.Δt
-        Δt_m = T.Δtimes[m]
-        times = T.times
-        t_m = times[m]
-        t_mp1 = times[m+1]
-        u_mk = u_vec_k[m]
-        u_mp1k = u_vec_k[m+1]
-        u_mkp1 = u_vec_kp1[m]
-
-        S_summand(q) = (S[m+1,q]-S[m,q])*f(times[q], u_vec_k[q])
-        S_sum = sum(S_summand, 1:size(S)[2])
-
-        RHS_factor = u_mkp1
-        RHS_factor += Δt_m*(Δf_exp(t_m, u_mkp1, u_mk)-f_imp(t_mp1, u_mp1k))
-        RHS_factor += S_sum
-
-        RHS_factor/LHS_factor(t_mp1, Δt_m)
-    end
-end
-
-function computeS(n::Int)
-    nodes, weights = gausslobatto(n);
-    nodes = 0.5*(nodes .+ 1.0)
-
+function make_spectral_int_matrix(n::Int)
+    nodes = gauss_lobatto_nodes(n) # Perhaps add option to change quadrature in the future
     S = zeros(n,n)
 
     for j = 1:n
@@ -297,31 +39,224 @@ function computeS(n::Int)
     return S
 end
 
-function main()
-    n = 4
-    t_n, Δt = 0.0, 1.0
-    u_n = u(t_n)
-    c_nodes = gauss_lobatto_nodes(n)
-    T = Timing(t_n, Δt, c_nodes)
-    #S = spectral_integration_matrix(c_nodes)
-    S = computeS(n)
+"""
+Notes for improvement:
 
-    u_np1_saves = perform_timestep(u_n, T, S)
-    graph(u_np1_saves, T, display_plot=true)
+The main usage for this function will probably be something like:
+for i in 1:number_of_timesteps
+    SDC(...)
+
+So I don't want to waste resources recomputing S everytime, or setting up all
+the equations.
+
+Perhaps I should have a create_SDC function? This would be provided all
+parameters except the initial guess, and create an SDC function which only
+needs the initial values as arguments.
+
+From a UX perspective, I like that they will have an SDC function whose only
+arguments are the initial data. That would be very easy to use. But I don't
+like that they have to call a function to create a function. That seems like it
+might be confusing.
+
+Actually, I think it would be good to also have Δt as an argument to the
+user-SDC function. I can imagine a user might want to use variable timestep
+size (but they probably don't need to vary the number of collocation nodes,
+implicit/explicit f, etc).
+"""
+function SDC(n, fE, fI, t, Δt, u, implicit_solver)
+    nodes = gauss_lobatto_nodes(n)
+    n_cor = 2*n-3
+    S = make_spectral_int_matrix(n) # nodes will be computed twice; should make more efficient
+    subtimes = (Δt*nodes).+ t 
+    subvals = Vector{typeof(u)}(undef, n)
+
+    subvals[1] = u
+    # Prediction
+    for m in 1:n-1
+        subvals[m+1] = begin
+            u_m = subvals[m]
+            t_m = subtimes[m]
+            t_mp1 = subtimes[m+1]
+            Δt_m = t_mp1 - t_m
+            RHS = u_m + Δt_m*fE(t_m,u_m)
+            implicit_solver(-Δt_m, t_m, RHS)
+        end
+    end
+
+    f(t, u) = fE(t, u) + fI(t, u)
+    # Correction
+    for k in 1:n_cor
+        # B15 - Not needed for gauss lobatto nodes, just leaves subvals as they were
+        # But B15 will be needed if start point is not a node
+        f_subvals = [f(subtimes[i], subvals[i]) for i in 1:n]
+        u_m_k = subvals[1]
+        for m in 1:n-1
+            #B16
+            u_m_k_next = subvals[m+1] # Should figure out better notation for this
+            subvals[m+1] = begin
+                u_m_kp1 = subvals[m]
+                t_m = subtimes[m]
+                t_mp1 = subtimes[m+1]
+                Δt_m = t_mp1 - t_m
+                u_mp1_k = subvals[m+1]
+                ΔfE = fE(t_m, u_m_kp1) - fE(t_m, u_m_k)
+                RHS = subvals[m] + Δt_m*(ΔfE - f(t_mp1, u_mp1_k))
+                RHS += sum(q -> (S[m+1,q]-S[m,q])*f_subvals[q], 1:size(S)[2])
+                implicit_solver(-Δt_m, fI, RHS)
+            end
+            u_m_k = u_m_k_next
+        end
+    end
+    # Will be different if endpoint is a node, need to use B6
+    return subvals[end]
 end
-#c_nodes = [0.0, 0.5, 1.0]
-#dir_name = "FigureSaves"
-#if !isdir(dir_name)
-#    mkdir(dir_name)
+
+
+"""
+This function returns a function: an SDC solver which needs only the initial
+conditions as the arguments. Moreover, the quadrature and spectral integration
+matrix are built-in to the function, not calculated each run.
+"""
+function make_SDC(n, fE, fI, implicit_solver)
+    nodes = gauss_lobatto_nodes(n)
+    n_cor = 2*n-3
+    S = make_spectral_int_matrix(n) # nodes will be computed twice; should make more efficient
+
+    return function small_arg_list_SDC(u, t, Δt)
+        subtimes = (Δt*nodes).+ t 
+        subvals = Vector{typeof(u)}(undef, n)
+        subvals[1] = u
+        # Prediction
+        for m in 1:n-1
+            subvals[m+1] = begin
+                u_m = subvals[m]
+                t_m = subtimes[m]
+                t_mp1 = subtimes[m+1]
+                Δt_m = t_mp1 - t_m
+                RHS = u_m + Δt_m*fE(t_m,u_m)
+                implicit_solver(-Δt_m, fI, RHS)
+            end
+        end
+
+        f(t, u) = fE(t, u) + fI(t, u)
+        # Correction
+        for k in 1:n_cor
+            # B15 - Not needed for gauss lobatto nodes, just leaves subvals as they were
+            # But B15 will be needed if start point is not a node
+            f_subvals = [f(subtimes[i], subvals[i]) for i in 1:n]
+            u_m_k = subvals[1]
+            for m in 1:n-1
+                #B16
+                u_m_k_next = subvals[m+1] # Should figure out better notation for this
+                subvals[m+1] = begin
+                    u_m_kp1 = subvals[m]
+                    t_m = subtimes[m]
+                    t_mp1 = subtimes[m+1]
+                    Δt_m = t_mp1 - t_m
+                    u_mp1_k = subvals[m+1]
+                    ΔfE = fE(t_m, u_m_kp1) - fE(t_m, u_m_k)
+                    RHS = subvals[m] + Δt_m*(ΔfE - f(t_mp1, u_mp1_k))
+                    RHS += sum(q -> (S[m+1,q]-S[m,q])*f_subvals[q], 1:size(S)[2])
+                    implicit_solver(-Δt_m, fI, RHS)
+                end
+                u_m_k = u_m_k_next
+            end
+        end
+        # Will be different if endpoint is a node, need to use B6
+        return subvals[end]
+    end
+end
+
+#function graph(u_np1_saves, T::Timing;
+#              graph_name::String="SDC_evolution.png",
+#              display_plot::Bool=false)
+#
+#    # Get exact value for comparison purposes
+#    u_exact = u(T.t_n + T.Δt)
+#    # Compute errors
+#    u_np1_saves_errors = Vector{Tuple{Int64, Float64}}(undef, length(u_np1_saves))
+#    for (i, (j, u_np1)) in enumerate(u_np1_saves)
+#        #u_np1_saves_errors[i] = (j, abs(u_np1-u_exact))
+#        u_np1_saves_errors[i] = (j, u_np1 - u_exact)
+#    end
+#
+#    # Plotting
+#    plot = Plots.plot(
+#        getindex.(u_np1_saves_errors, 1), # No of Corrections
+#        getindex.(u_np1_saves_errors, 2), # Error
+#        label="Approximation of u(t_n+Δt)"
+#    )
+#    p = length(T.Δtimes)+1
+#    Plots.plot!(
+#        plot,
+#        title="Error Over 1 Timestep Vs No of Corrections \nλ=$λ, Δt=$(T.Δt), t_n=$(T.t_n), p=$p (Gauss-Lobatto)",
+#        xlabel="# of Corrections (0 ⟹ Initial Prediction)",
+#        ylabel="Error",
+#        xlim=(0, 2*p),
+#        #yaxis=:log,
+#    )
+#    # Make a line on y=0, for easier comprehension
+#    Plots.hline!(plot, [0], label="")
+#
+#    Plots.savefig(plot, graph_name)
+#
+#    if display_plot
+#        display(plot)
+#    end
+#    return nothing
 #end
-#no_of_runs = 50
-#rand_λs = -5*rand(Float64, no_of_runs)
-#rand_Δts = 2*rand(Float64, no_of_runs)
-#rand_t_ns = 5*rand(Float64, no_of_runs)
-#for (i, (λ, Δt, t_n)) in enumerate(zip(rand_λs, rand_Δts, rand_t_ns))
-#    myround(x) = round(x, digits=2)
-#    λ, Δt, t_n = myround(λ), myround(Δt), myround(t_n)
-#    main(λ, Δt, t_n, c_nodes, graph_name="$dir_name/figure$i.png", display_plot=false)
+
+
+#"""
+#Make sure collocation nodes have proper scale and order.
+#"""
+#function validate_collocation_nodes(c_nodes)
+#    for node in c_nodes
+#        if node < 0 || node > 1
+#            throw(DomainError(node, "Collocation nodes must be ∈ [0,1]."))
+#        end
+#    end
+#    for i in 1:length(c_nodes)-1
+#        if c_nodes[i+1] <= c_nodes[i]
+#            throw(DomainError(node, "Collocation nodes must be obey c_m+1 > c_m ∀ m = 1, ... , p."))
+#        end
+#    end
 #end
+
+"""
+Currently a demonstration of SDC solving
+"""
+function main()
+
+    # 5-node quadrature
+    n = 5
+    # IMEX Euler, half explicit, half implicit (dalhquist equation)
+    fE(t, u) = 0.5*u
+    fI(t, u) = 0.5*u
+    # Solve implicit euler analytically
+    implicit_solver(α, t, b) = b/(1+α)
+
+    my_SDC = make_SDC(n, fE, fI, implicit_solver)
+
+    u(t) = exp(t)
+    t = 0.0
+    Δtimes = [1.0, 0.5, 1.5, 1.1414, 0.7071]
+    times = cumsum(Δtimes) .+ t
+
+    u_actual = []
+    u_approx = []
+    u_n = u(t)
+    for i in 1:length(Δtimes)
+        t += Δtimes[i] 
+        u_n = my_SDC(u_n, t, Δtimes[i])
+        push!(u_actual, u(t))
+        push!(u_approx, u_n)
+    end
+
+    u_diff = abs.(u_actual - u_approx)
+    display(u_diff)
+
+    Plots.plot(times, u_diff, yaxis=:log, display_plot=true)
+end
 
 main()
